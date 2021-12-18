0902fab24ad668fa46fd673849c2520344845283
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-18 13:20:38 -0500

    animations practice

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Layout from './layouts/Layout';
 
 function App() {
   return (
     <div className="App">
-
+      <Layout >
+      </Layout>
+      <div className='lined thin'>
+        <h4>- take out the trash</h4>
+        <h4>- do the dishes</h4>
+        <h4>- update resume</h4>
+      </div>
+      <div className='gradient'>
+        
+      </div>
     </div>
   );
 }
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import './Layout.css';
 
 export default function Layout({children}) {
     return (
         <div>
-            <p>Layout</p>
+            <nav>
+                <h3>Projects</h3>
+                <h3>About Me</h3>
+                <h3>Contact</h3>
+                <h3>Testimonials</h3>
+            </nav>
         </div>
     )
 }
