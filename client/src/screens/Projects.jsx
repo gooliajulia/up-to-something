6b146addcb15bb82921c1bad89536276c32ff944
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-21 08:44:42 -0500

    project detail and image sizing css

--- client/src/screens/Projects.jsx
+++ client/src/screens/Projects.jsx
@@ -9,13 +9,13 @@ export default function Projects() {
                 <div className='project-details'>
                     <h3>Project Name: Foodies.</h3>
                     <h4>Deployed site: {<a href='https://foodies-community.netlify.app/' target="_blank">Foodies App</a>}</h4>
-                    <h5>...type description here...</h5>
-                    <h5>time to complete</h5>
-                    <h5>Mission</h5>
-                    <h5>tech used</h5>
-                    <h5>GitHub link</h5>
-                    <h5>lessons learned</h5>
-                    <h5>future development</h5>
+                    <h5 className='description'>The Foodies app is a Spotify mimic that replaces songs with recipes and playlists with meal plans. Modern day recipe hunting and meal planning is clunky and nonsocial. Foodies solves this problem by not only allowing users to connect and follow fellow foodies for recipe inspiration, but store all of their favorite recipes in one place. </h5>
+                    <h5>Time to Complete: 5 days</h5>
+                    <h5>Mission:</h5>
+                    <h5>Tech used: React, Ruby on Rails</h5>
+                    <h5>GitHub Link: </h5>
+                    <h5>Lessons Learned:</h5>
+                    <h5>Future Development:</h5>
                 </div>
             </div>
         </div>
--- client/src/screens/WhoIAm.jsx
+++ client/src/screens/WhoIAm.jsx
@@ -4,6 +4,7 @@ export default function WhoIAm() {
     return (
         <div>
             <p> Who I Am</p>
+            <p>My values as a developer: minimalism (quality over quanity; smart, intuitive user focused experience; effortless user experience; customizable elements to make the user felt seen, understanding themselves better, grow, explore and play in a way that is just so THEM</p>
         </div>
     )
 }
