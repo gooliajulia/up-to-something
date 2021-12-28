5af428dffa102c83e0968303a2e6454c92c0bed3
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-27 20:42:58 -0500

    update script build command

--- client/src/screens/Projects.jsx
+++ client/src/screens/Projects.jsx
@@ -5,7 +5,12 @@ export default function Projects() {
     return (
         <div className='projects'>
             <div className='project'>
+                <div className='images'>
                 <img src='../FoodiesApp.png' alt='Foodies App Screenshot' />
+                <img className='mobile' src='../ResponsiveC.png' alt='Mobile Design' />
+                <img className='tablet' src='../ResponsiveB.png' alt='Tablet Design' />
+                </div>
+
                 <div className='project-details'>
                     <h3>Project Name: Foodies.</h3>
                     <h4><span className='bold'>Deployed site:</span> <a href='https://foodies-community.netlify.app/' target="_blank">Foodies App</a></h4>
@@ -20,8 +25,14 @@ export default function Projects() {
                     <br/>
                     <h5><span className='bold'>Tech used: </span>React, Ruby on Rails, React Router, Netflify and Heroku Deployment</h5>
                     <h5><span className='bold'>GitHub Link: <a href='https://github.com/gooliajulia/foodies/blob/main/README.md' target="_blank" >Foodies App GitHub Repo</a></span></h5>
-                    <h5><span className='bold'>Lessons Learned: this project challenged me to design a dynamic layout with left side nav, and a right side feed. I created a main container to house the changing components as children of the layout and to hold much of the state. Then there was the added challenging of problem-solving a responsive design on smaller screens. This project helped me strengthen my understanding of how react works, and appreciate just how dynamic it can be.</span></h5>
-                    <h5><span className='bold'>Future Development:</span></h5>
+                    <h5><span className='bold'>Lessons Learned: </span>this project challenged me to design a dynamic layout with left side nav, and a right side feed. I created a main container to house the changing components as children of the layout and to hold much of the state. Then there was the added challenging of problem-solving a responsive design on smaller screens. This project helped me strengthen my understanding of how react works, and appreciate just how dynamic it can be.</h5>
+                    <h5><span className='bold'>Future Development:</span>
+                    <ul>
+                        <li>Add ingredients and benefits resources on backend. Ingredients can have many benefits and vice versa, so that users can sort and search for recipes based on ingredients or benefits</li>
+                        <li>Allow user's to create daily 'Meal Plans' consisting of recipes for the day. For example a meal plan for the day before a marathon, for hosting guests, or for a typical work day.</li>
+                        <li>More Home Page features that allow user to explore and discover new recipes including a 'What should I eat?' quiz, 'Recomended for you' section, or different countries highlight</li>
+                        <li>Allow user to track their own fridge and pantry items, create a grocery list, and search recipes based on what ingredients they have on hand</li>
+                    </ul></h5>
                 </div>
             </div>
         </div>
