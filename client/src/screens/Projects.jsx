4aaa2216f172d584640a4a7b15a44505f4a26ad4
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-29 15:30:46 -0500

    Projects 2 and 3 quick add

--- client/src/screens/Projects.jsx
+++ client/src/screens/Projects.jsx
@@ -35,6 +35,69 @@ export default function Projects() {
                     </ul></h5>
                 </div>
             </div>
+            <div className='project'>
+                <div className='images'>
+                    <img src='../DesignMyWeekHome.png' alt='Foodies App Screenshot' />
+                    <img className='mobile' src='../DesignMyWeekAddTask.png' alt='Mobile Design' />
+                    <img className='tablet' src='../DesignMyWeekSchedule.png' alt='Tablet Design' />
+                </div>
+
+                <div className='project-details'>
+                    <h3>Project Name: Design My Week</h3>
+                    <h4><span className='bold'>Deployed site:</span> <a href='https://fervent-swartz-34d852.netlify.app/' target="_blank">Design My Week</a></h4>
+                    <h5 className='description'>Design My Week is </h5>
+                    <h5><span className='bold'>Time to Complete:</span> 6 days</h5>
+                    <h5 className='mission'><span className='bold'>Mission: </span></h5>
+                    <ul>
+                        <li>Build a ...</li>
+                        <li>...Full CRUD generic controller actions on Back End </li>
+                        <li>,,,React App Front End with full CRUD functionality, consuming data from Ruby on Rails API</li>
+                    </ul>
+                    <br/>
+                    <h5><span className='bold'>Tech used: </span>React, Ruby on Rails, React Router, Netflify and Heroku Deployment</h5>
+                    <h5><span className='bold'>GitHub Link: <a href='https://github.com/gooliajulia/foodies/blob/main/README.md' target="_blank" >Foodies App GitHub Repo</a></span></h5>
+                    <h5><span className='bold'>Lessons Learned: </span>this project challenged me to design a dynamic layout with left side nav, and a right side feed. I created a main container to house the changing components as children of the layout and to hold much of the state. Then there was the added challenging of problem-solving a responsive design on smaller screens. This project helped me strengthen my understanding of how react works, and appreciate just how dynamic it can be.</h5>
+                    <h5><span className='bold'>Future Development:</span>
+                    <ul>
+                        <li>Add ingredients and benefits resources on backend. Ingredients can have many benefits and vice versa, so that users can sort and search for recipes based on ingredients or benefits</li>
+                        <li>Allow user's to create daily 'Meal Plans' consisting of recipes for the day. For example a meal plan for the day before a marathon, for hosting guests, or for a typical work day.</li>
+                        <li>More Home Page features that allow user to explore and discover new recipes including a 'What should I eat?' quiz, 'Recomended for you' section, or different countries highlight</li>
+                        <li>Allow user to track their own fridge and pantry items, create a grocery list, and search recipes based on what ingredients they have on hand</li>
+                    </ul></h5>
+                </div>
+            </div>
+            <div className='project'>
+                <div className='images'>
+                    <img src='../SunshineHome.png' alt='Foodies App Screenshot' />
+                    <img className='mobile' src='../SunshineHighlight.png' alt='Mobile Design' />
+                    <img className='tablet' src='../SunshineFruitPunch.png' alt='Tablet Design' />
+                </div>
+
+                <div className='project-details'>
+                    <h3>Project Name: Sunshine Smoothies</h3>
+                    <h4><span className='bold'>Deployed site:</span> <a href='https://sunshine-smoothies.netlify.app/' target="_blank">Sunshine Smoothies</a></h4>
+                    <h5 className='description'>Sunshine Smoothies is </h5>
+                    <h5><span className='bold'>Time to Complete:</span> 7 days</h5>
+                    <h5 className='mission'><span className='bold'>Mission: </span></h5>
+                    <ul>
+                        <li>Build a ...</li>
+                        <li>...Full CRUD generic controller actions on Back End </li>
+                        <li>,,,React App Front End with full CRUD functionality, consuming data from Ruby on Rails API</li>
+                    </ul>
+                    <br/>
+                    <h5><span className='bold'>Tech used: </span>React, Ruby on Rails, React Router, Netflify and Heroku Deployment</h5>
+                    <h5><span className='bold'>GitHub Link: <a href='https://github.com/gooliajulia/foodies/blob/main/README.md' target="_blank" >Foodies App GitHub Repo</a></span></h5>
+                    <h5><span className='bold'>Lessons Learned: </span>this project challenged me to design a dynamic layout with left side nav, and a right side feed. I created a main container to house the changing components as children of the layout and to hold much of the state. Then there was the added challenging of problem-solving a responsive design on smaller screens. This project helped me strengthen my understanding of how react works, and appreciate just how dynamic it can be.</h5>
+                    <h5><span className='bold'>Future Development:</span>
+                    <ul>
+                        <li>Add ingredients and benefits resources on backend. Ingredients can have many benefits and vice versa, so that users can sort and search for recipes based on ingredients or benefits</li>
+                        <li>Allow user's to create daily 'Meal Plans' consisting of recipes for the day. For example a meal plan for the day before a marathon, for hosting guests, or for a typical work day.</li>
+                        <li>More Home Page features that allow user to explore and discover new recipes including a 'What should I eat?' quiz, 'Recomended for you' section, or different countries highlight</li>
+                        <li>Allow user to track their own fridge and pantry items, create a grocery list, and search recipes based on what ingredients they have on hand</li>
+                    </ul></h5>
+                </div>
+            </div>
+
         </div>
     )
 }
