eaab4960437316b449e0619a6686cdc42f8da418
gooliajulia929 <juliadwyer93@gmail.com>
2021-12-29 19:57:24 -0500

    fix routes on home

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path='/projects' >
           <Projects />
         </Route>
-        <Route path='/about-me'>
+        <Route path='/about-me' exact >
           <WhoIAm />
         </Route>
         <Route path='/contact'>
@@ -22,7 +22,7 @@ function App() {
           <p>Julia Dwyer Personal Brand Statement:
           “A thoughtful creative who is passionate about problem solving. As a developer I’m eager to learn and always looking for a new challenge. With a combined background in engineering and the arts, I’m passionate about combining the two to create intuitive, enjoyable and optimized experiences and products. I value working positively on a team and am excited to see what the future holds.”</p>
         </Route>
-        <Route path='/'>
+        <Route path='/' exact>
           <WhoIAm />
         </Route>
       </Layout>
